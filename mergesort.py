fc461a1f31fe083f50b599394fbcabf99aa01f8b
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2019-02-27 21:21:06 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,23 +1,27 @@
 def merge(left,right):
     result = []
+    i , j = 0,0
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             result.append(left[i])
-            i++
+            i +=1
         else:
             result.append(right[j])
-            j++
+            j +=1
     result +=left[i:]
     result +=right[j:]
+    return result
 def mergesort(lst):
-    if len(lst) <=:
+    if len(lst) <=1:
         return lst
-    mid = int(len(lst))//2
+    mid = len(lst)//2
     left = mergesort(lst[:mid])
-    right = mergeg(lst[mid:])
-    return merge(left, right)
-arr = [1,23,-1,4,30,-6,-2,22,33,0]
-print(mergesort(arr))
+    right = mergesort(lst[mid:])
+    sorted_array = merge(left, right)
+    return sorted_array
+
+lst = [1,23,-1,4,30,-6,-2,22,33,0]
+print(mergesort(lst))
 
     
         
